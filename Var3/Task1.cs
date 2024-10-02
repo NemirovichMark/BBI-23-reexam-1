17e57d12c25a427cd9c903e2bc7bf2cd5662e391
Rean-Lyaya <159897712+Rean-Lyaya@users.noreply.github.com>
2024-10-02 11:56:03 +0300

    Add files via upload

---
+++ Var3/Task1.cs
@@ -0,0 +1,89 @@
+﻿public class Task1
+{
+    public struct Dot
+    {
+        private double x, y, z;
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y; } set { y = value; } }
+        public double Z { get { return z; } set { z = value; } }
+        public Dot(double[] coordinates)
+        {
+            if (coordinates.Length == 3)
+            {
+                x = coordinates[0];
+                y = coordinates[1];
+                z = coordinates[2];
+            }
+            else
+            {
+                x = 0;
+                y = 0;
+                z = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"x = {x}, y = {y}, z = {z}";
+        }
+    }
+
+    public struct Vector
+    {
+        private Dot start, end;
+        public Dot Start { get { return start; } set { start = value; } }
+        public Dot End { get { return end; } set { end = value; } }
+
+        public Vector(Dot start, Dot end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public double GetLength()
+        {
+            return Math.Sqrt(Math.Pow(end.X - start.X, 2) + Math.Pow(end.Y - start.Y, 2) + Math.Pow(end.Z - start.Z, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"{start}\n{end}\nlength = {GetLength()}";
+        }
+    }
+
+    private Vector[] vectors;
+
+    public Vector[] Vectors { get { return vectors; } }
+
+    public Task1(Vector[] vectors)
+    {
+        this.vectors = vectors;
+    }
+
+    public void Sort()
+    {
+        int i = 1;
+        int j = 2;
+
+        while (i < vectors.Length)
+        {
+            if (vectors[i - 1].GetLength() <= vectors[i].GetLength())
+            {
+                i = j;
+                j++;
+            }
+            else
+            {
+                var temp = vectors[i - 1];
+                vectors[i - 1] = vectors[i];
+                vectors[i] = temp;
+                i--;
+                if (i == 0)
+                {
+                    i = j;
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ Var3/Task2.cs
@@ -0,0 +1,87 @@
+﻿using static Task1;
+using System;
+
+public class Task2
+{
+    public abstract class Shape
+    {
+        private Dot center;
+        private Vector pointer;
+        public Dot Center { get { return center; } }
+        public Vector Pointer { get { return pointer; } }
+
+        public Shape(Dot center)
+        {
+            this.center = center;
+        }
+
+        public void CreateVector(Dot point)
+        {
+            pointer = new Vector(Center, point);
+        }
+
+        public abstract double Volume();
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} with V = {Volume():F2}";
+        }
+    }
+
+    public class Sphere : Shape
+    {
+        public Sphere(Dot center) : base(center) { }
+
+        public override double Volume()
+        {
+            double radius = Pointer.GetLength();
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+
+    public class Cube : Shape
+    {
+        public Cube(Dot center) : base(center) { }
+
+        public override double Volume()
+        {
+            double edge = Pointer.GetLength();
+            return Math.Pow(edge, 3);
+        }
+    }
+
+    private Shape[] _shapes;
+
+    public Shape[] Shapes => _shapes;
+
+    public Task2(Shape[] shapes)
+    {
+        _shapes = shapes;
+    }
+
+    public void Sorting()
+    {
+        for (int i = 1; i < _shapes.Length; i++)
+        {
+            var key = _shapes[i];
+            int j = i - 1;
+
+            while (j >= 0 && _shapes[j].Volume() > key.Volume())
+            {
+                _shapes[j + 1] = _shapes[j];
+                j = j - 1;
+            }
+            _shapes[j + 1] = key;
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        foreach (var shape in _shapes)
+        {
+            result += shape + "\n";
+        }
+        return result;
+    }
+}
