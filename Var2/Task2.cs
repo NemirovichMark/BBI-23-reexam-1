b5ad2e5a07f6270ca0aeef31a7807093949f6882
Чернышов Илья <159529876+ChernyshovIlya@users.noreply.github.com>
2024-10-02 11:59:58 +0300

    Add files via upload

---
+++ Var2/Task2.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Variant_2.Task2;
+
+//namespace Variant_2
+//{
+//    public class Task2
+//    {
+//        public struct Point
+//        {
+
+//        }
+//        abstract public class Fourangle
+//        {
+
+//        }
+
+//        public Task2(Point[] points)
+//        {
+
+//        }
+
+//        public class Square
+//        {
+
+//        }
+
+//        public class Rectangle
+//        {
+
+//        }
+//        public Task2(Fourangle[] fourangles)
+//        {
+
+//        }
+
+//    }
+//}
+
+namespace Variant_2
+{
+    public class Task2
+    {
+        public struct Point
+        {
+            private double x;
+            private double y;
+            private double[] doubles;
+
+            public double X => x;
+            public double Y => y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public Point(double[] doubles) : this()
+            {
+                this.doubles = doubles;
+            }
+
+            public override string ToString()
+            {
+                return $"x = {x}, y = {y}";
+            }
+
+            public double Length(Point other)
+            {
+                return Math.Round(Math.Sqrt(Math.Pow(other.x - x, 2) + Math.Pow(other.y - y, 2)), 2);
+            }
+        }
+
+        abstract public class Fourangle
+        {
+            protected Point[] points;
+
+            public Fourangle(Point[] points)
+            {
+                if (points.Length == 4)
+                {
+                    this.points = points;
+                }
+                else
+                {
+                    this.points = new Point[4];
+                }
+            }
+
+            public abstract double Length();
+            public abstract double Area();
+
+            public override string ToString()
+            {
+                return $"Type: {this.GetType().Name}, P = {Length()}, S = {Area()}";
+            }
+        }
+
+        public class Square : Fourangle
+        {
+            public Square(Point[] points) : base(points) { }
+
+            public override double Length()
+            {
+                return 4 * points[0].Length(points[1]);
+            }
+
+            public override double Area()
+            {
+                return Math.Pow(points[0].Length(points[1]), 2);
+            }
+        }
+
+        public class Rectangle : Fourangle
+        {
+            public Rectangle(Point[] points) : base(points) { }
+
+            public override double Length()
+            {
+                return 2 * (points[0].Length(points[1]) + points[1].Length(points[2]));
+            }
+
+            public override double Area()
+            {
+                return points[0].Length(points[1]) * points[1].Length(points[2]);
+            }
+        }
+
+        private Fourangle[] fourangles;
+
+        public Fourangle[] Fourangles => fourangles;
+
+        public Task2(Point[] points)
+        {
+
+        }
+
+        public Task2(Fourangle[] fourangles)
+        {
+            this.fourangles = fourangles;
+        }
+
+        public void Sorting()
+        {
+            Array.Sort(fourangles, (f1, f2) => f2.Area().CompareTo(f1.Area()));
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", fourangles.Select(f => f.ToString()));
+        }
+    }
+}
---
+++ Var2/Task4.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static Variant_2.Task3;
+#region Выберите библиотеку(и) для сериализации
+// using Newtonsoft;
+//using System.Text.Json;
+using System.Text.Json.Serialization;
+#endregion
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static Variant_2.Task3;
+
+namespace Variant_2
+{
+    public class Task4
+    {
+        public interface ICreator
+        {
+            void CreateFolder(string path, string folderName);
+
+            void CreateFolders(string path, string[] folderNames);
+
+            void CreateFile(string path, string fileName);
+
+            void CreateFiles(string path, string[] fileNames);
+        }
+
+        public interface IDataSerializer
+        {
+            void Write<T>(string path, T obj);
+
+            T Read<T>(string path);
+        }
+
+        public class DataSerializer : ICreator, IDataSerializer
+        {
+            public void CreateFolder(string path, string folderName)
+            {
+                Directory.CreateDirectory(Path.Combine(path, folderName));
+            }
+
+            public void CreateFolders(string path, string[] folderNames)
+            {
+                foreach (var folderName in folderNames)
+                {
+                    CreateFolder(path, folderName);
+                }
+            }
+
+            public void CreateFile(string path, string fileName)
+            {
+                using (File.Create(Path.Combine(path, fileName))) { }
+            }
+
+            public void CreateFiles(string path, string[] fileNames)
+            {
+                foreach (var fileName in fileNames)
+                {
+                    CreateFile(path, fileName);
+                }
+            }
+
+            public void Write<T>(string path, T obj)
+            {
+                var json = JsonSerializer.Serialize(obj);
+                File.WriteAllText(path, json);
+            }
+
+            public T Read<T>(string path)
+            {
+                var json = File.ReadAllText(path);
+                return JsonSerializer.Deserialize<T>(json);
+            }
+
+            public void CreateFolder(string path, string[] strings)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void CreateFile(string path, string[] strings)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Write(Grep greper, string path)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private Grep greper;
+
+        public Grep Greper => greper;
+
+        public Task4(Grep grep)
+        {
+            greper = grep;
+        }
+    }
+}
