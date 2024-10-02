b77be82566b3f8adbae900320ac3d8eb74907503
VladimirKostylev <146061973+VladimirKostylev@users.noreply.github.com>
2024-10-02 12:01:05 +0300

    Add files via upload

---
+++ Var2/Task1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_2
+{
+    public class Task1
+    {
+        public struct Point
+        {
+            private int x;
+            private int y;
+
+            public int X { get => x; }
+            public int Y { get => y; }
+
+            public Point(int[] coordinates)
+            {
+                if (coordinates.Length == 2)
+                {
+                    x = coordinates[0];
+                    y = coordinates[1];
+                }
+                else
+                {
+                    x = 0;
+                    y = 0;
+                }
+            }
+
+            public override string ToString()
+            {
+                return "x = " + x + " y = " + y;
+            }
+
+            public double Length(Point point)
+            {
+                return Math.Round(Math.Sqrt(Math.Pow(point.X - X, 2) + Math.Pow(point.Y - Y, 2)), 2);
+            }
+
+            public static string Distance(Point point_1, Point point_2)
+            {
+                return point_1 + "\n" + point_2 + "\n" + "Distance = " + point_1.Length(point_2);
+            }
+        }
+
+        private Point[] points;
+
+        public Point[] Points { get => points; }
+
+        public Task1(Point[] points)
+        {
+            if (points.Length == 2)
+            {
+                this.points = points;
+            }
+            else
+            {
+                this.points = new Point[] { new Point(new int[] { 0, 0 }), new Point(new int[] { 0, 0 }) };
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var point in points)
+            {
+                result += point + "\n";
+            }
+            return result;
+        }
+
+        public void Sorting()
+        {
+            int i = 0;
+            while (i < points.Length)
+            {
+                if (i == 0 || points[i].Length(new Point(new int[] { 0, 0 })) >= points[i - 1].Length(new Point(new int[] { 0, 0 })))
+                {
+                    i++;
+                }
+                else
+                {
+                    Point temp = points[i];
+                    points[i] = points[i - 1];
+                    points[i - 1] = temp;
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ Var2/Task2.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static Variant_2.Task2;
+
+namespace Variant_2
+{
+    public class Task2
+    {
+        public struct Point
+        {
+            public double x;
+            public double y;
+
+            public double X
+            {
+                get { return x; }
+            }
+
+            public double Y
+            {
+                get { return y; }
+            }
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public override string ToString()
+            {
+                return $"x = {x}, y = {y}";
+            }
+
+            public double Length(Point other)
+            {
+                double distance = Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2));
+                return Math.Round(distance, 2);
+            }
+
+            public static string GetInfo(Point point1, Point point2)
+            {
+                string info = $"Точка 1: {point1}nТочка 2: {point2}nРасстояние между точками: {point1.Length(point2)}";
+                return info;
+            }
+        }
+
+        abstract public class Fourangle
+        {
+            protected Point[] points;
+
+            public Fourangle(Point[] points)
+            {
+                if (points.Length != 4)
+                {
+                    this.points = new Point[4];
+                }
+                else
+                {
+                    this.points = points;
+                }
+            }
+
+            public abstract double Length();
+            public abstract double Area();
+
+            public override string ToString()
+            {
+                return $"{GetType().Name} with P = {Length():F2}, S = {Area():F2}";
+            }
+        }
+
+        public Task2(Point[] points)
+        {
+         
+        }
+
+        public class Square : Fourangle
+        {
+            public Square(Point[] points) : base(points) { }
+
+            public override double Length()
+            {
+                double side = points[0].Length(points[1]);
+                return side * 4;
+            }
+
+            public override double Area()
+            {
+                double side = points[0].Length(points[1]);
+                return side * side;
+            }
+        }
+
+        public class Rectangle : Fourangle
+        {
+            public Rectangle(Point[] points) : base(points) { }
+
+            public override double Length()
+            {
+                double width = points[0].Length(points[1]); // Ширина прямоугольника
+                double height = points[1].Length(points[2]); // Высота прямоугольника
+                return 2 * (width + height);
+            }
+
+            public override double Area()
+            {
+                double width = points[0].Length(points[1]);
+                double height = points[1].Length(points[2]);
+                return width * height;
+            }
+        }
+
+        private Fourangle[] fourangles;
+
+        public Fourangle[] Fourangles
+        {
+            get { return fourangles; }
+        }
+
+        public Task2(Fourangle[] fourangles)
+        {
+            this.fourangles = fourangles;
+        }
+
+        public void Sorting()
+        {
+            Array.Sort(fourangles, (f1, f2) => f2.Area().CompareTo(f1.Area())); // Сортировка по убыванию площади
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Fourangle fourangle in fourangles)
+            {
+                output += fourangle.ToString() + "n";
+            }
+            return output;
+        }
+    }
+}
