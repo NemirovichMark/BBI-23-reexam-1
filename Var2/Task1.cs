e98ec4a731c522ae27023e3f61a921475e94a657
KarinaAbramenkova <146069361+KarinaAbramenkova@users.noreply.github.com>
2024-10-02 12:06:30 +0300

    Update Task1.cs
    Я не сохранила файл, поэтому поменяла.

--- Var2/Task1.cs
+++ Var2/Task1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
@@ -9,15 +9,89 @@ namespace Variant_2
 {
     public class Task1
     {
-        public struct Point
+        private Point[] points;
+
+        public Point[] Points => points;
+
+        public Task1(Point[] pointsArray)
         {
-           
+            if (pointsArray.Length != 2)
+                throw new ArgumentException("Массив должен содержать ровно 2 точки.");
+            this.points = pointsArray;
         }
 
-        public Task1(Point[] points)
+        public override string ToString()
         {
-
+            string result = "";
+            foreach (var point in points)
+            {
+                result += point.ToString() + "\n";
+            }
+            return result.TrimEnd();
         }
 
+        public void Sorting()
+        {
+            ShellSort(points);
+        }
+
+        private void ShellSort(Point[] array)
+        {
+            int n = array.Length;
+            int gap = n / 2;
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    Point temp = array[i];
+                    double tempDistance = temp.DistanceFromOrigin();
+                    int j;
+                    for (j = i; j >= gap && array[j - gap].DistanceFromOrigin() > tempDistance; j -= gap)
+                    {
+                        array[j] = array[j - gap];
+                    }
+                    array[j] = temp;
+                }
+                gap /= 2;
+            }
+        }
+
+    }
+    public struct Point
+    {
+        private double x;
+        private double y;
+
+        public double X => x;
+        public double Y => y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"x = {x}, y = {y}";
+        }
+
+        public double Length(Point other)
+        {
+            double distance = Math.Sqrt(Math.Pow(other.X - this.X, 2) + Math.Pow(other.Y - this.Y, 2));
+            return Math.Round(distance, 2);
+        }
+
+        public static string ComparePoints(Point p1, Point p2)
+        {
+            double distance = p1.Length(p2);
+            return $"{p1}\n{p2}\nDistance: {distance}";
+        }
+
+        public double DistanceFromOrigin()
+        {
+            return Length(new Point(0, 0));
+        }
     }
 }
