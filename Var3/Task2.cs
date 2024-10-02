1e1c9e989fc53520b3017dda2cc7693585857633
TrofimovPlaton <159530222+TrofimovPlaton@users.noreply.github.com>
2024-10-02 11:53:09 +0300

    Add files via upload

---
+++ Var3/Task2.cs
@@ -0,0 +1,87 @@
+﻿using static Task1;
+public class Task2
+{
+    public abstract class Shape
+    {
+        public Dot Center { get; }
+        public Vector Pointer { get; private set; }
+
+        protected Shape(Dot center)
+        {
+            Center = center;
+        }
+
+        public void CreateVector(Dot point)
+        {
+            Pointer = new Vector(Center, point);
+        }
+
+        public abstract double Volume();
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} with V = {Volume():F2}";
+        }
+    }
+
+    public class Sphere : Shape
+    {
+        public Sphere(Dot center) : base(center) { }
+
+        public override double Volume()
+        {
+            double radius = Pointer.Length();
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+
+    public class Cube : Shape
+    {
+        public Cube(Dot center) : base(center) { }
+
+        public override double Volume()
+        {
+            double side = Pointer.Length();
+            return Math.Pow(side, 3);
+        }
+    }
+
+    private Shape[] shapes; 
+
+    public Shape[] Shapes => shapes; 
+
+    public Task2(Shape[] inputShapes)
+    {
+        shapes = new Shape[inputShapes.Length];
+        for (int i = 0; i < inputShapes.Length; i++)
+        {
+            shapes[i] = inputShapes[i];
+        }
+    }
+
+    public void Sorting()
+    {
+        for (int i = 0; i < shapes.Length - 1; i++)
+        {
+            for (int j = 0; j < shapes.Length - 1 - i; j++)
+            {
+                if (shapes[j].Volume() > shapes[j + 1].Volume())
+                {
+                    Shape temp = shapes[j];
+                    shapes[j] = shapes[j + 1];
+                    shapes[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        foreach (var shape in shapes)
+        {
+            result += shape.ToString() + "\n";
+        }
+        return result;
+    }
+}
---
+++ Var3/Task4.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static Task3;
+#region Выберите библиотеку(и) для сериализации
+using System.Text.Json;
+#endregion
+namespace Variant_3
+{
+    public class Task4
+    {
+        public interface ICreator
+        {
+            void CreateFolder(string path, string folderName);
+            void CreateFile(string path, string fileName);
+            void CreateFolders(string basePath, string[] folderNames);
+            void CreateFiles(string basePath, string[] fileNames);
+        }
+
+        public abstract class SerializerInFormate
+        {
+            public abstract void Write<T>(T obj, string path);
+            public abstract T Read<T>(string path);
+        }
+
+        public class SerializerInJson : SerializerInFormate, ICreator
+        {
+            public override void Write<T>(T obj, string path)
+            {
+                var jsonString = JsonSerializer.Serialize(obj);
+                File.WriteAllText(path, jsonString);
+            }
+
+            public override T Read<T>(string path)
+            {
+                var jsonString = File.ReadAllText(path);
+                return JsonSerializer.Deserialize<T>(jsonString);
+            }
+
+            public void CreateFolder(string path, string folderName)
+            {
+                Directory.CreateDirectory(Path.Combine(path, folderName));
+            }
+
+            public void CreateFile(string path, string fileName)
+            {
+                File.Create(Path.Combine(path, fileName)).Dispose(); 
+            }
+
+            public void CreateFolders(string basePath, string[] folderNames)
+            {
+                foreach (var folderName in folderNames)
+                {
+                    CreateFolder(basePath, folderName);
+                }
+            }
+
+            public void CreateFiles(string basePath, string[] fileNames)
+            {
+                foreach (var fileName in fileNames)
+                {
+                    CreateFile(basePath, fileName);
+                }
+            }
+        }
+
+        private SerializerInJson serializer; 
+        private Searcher searcher; 
+
+        public Task4(Searcher searcher)
+        {
+            this.searcher = searcher;
+            this.serializer = new SerializerInJson();
+        }
+
+        public void SaveToFile(string path)
+        {
+            serializer.Write(searcher, path);
+        }
+
+        public Searcher LoadFromFile(string path)
+        {
+            return serializer.Read<Searcher>(path);
+        }
+    }
+}
