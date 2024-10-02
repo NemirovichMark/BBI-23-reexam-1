fc4b0b44dda0d2ee9e7a99357e59f8081d6e4187
KarinaAbramenkova <146069361+KarinaAbramenkova@users.noreply.github.com>
2024-10-02 11:56:29 +0300

    Add files via upload

---
+++ Var2/Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variant_2
+{
+    public class Task1
+    {
+        public struct Point
+        {
+           
+        }
+
+        public Task1(Point[] points)
+        {
+
+        }
+
+    }
+}
